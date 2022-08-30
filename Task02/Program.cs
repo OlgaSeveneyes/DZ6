e1adb6c63a41475f6518f06e79b886ab355db477
Olga <osa_007@mail.ru>
2022-08-30 16:56:17 +0300

    added task2 for dz6

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -6,28 +6,9 @@ int b1 = NumberFromUser ("Введите b1: ","Ошибка ввода!");
 int k1 = NumberFromUser ("Введите k1: ","Ошибка ввода!");
 int b2 = NumberFromUser ("Введите b2: ","Ошибка ввода!");
 int k2 = NumberFromUser ("Введите k2: ","Ошибка ввода!");
-
-double point = PointResult (b1,k1,b2,k2);
-Console.WriteLine($"Прямые пересекаются в точке с координатами ({x};{y})");
-
-double PointResult (int bu1, int ku1, int bu2, int ku2)
-{
-double x, y, b, k;
-b = bu2-bu1;
-k = ku1-ku2;
-x = b/k;
-y = ku1*x + bu1;
-}
-
-/*int pointOfIntersection = PointOfIntersectionLines (b1, k1, b2, k2);
-Console.WriteLine($"Прямые пересекаются в точке {pointOfIntersection}");
-
-int PointOfIntersectionLines (int bum1, int kum1, int bum2, int kum2)
-{
-    int x = (bum2-bum1)/(kum2-kum1);
-    int y = kum1*x + bum1;
-    return (x, y);
-}*/
+double point1 = PointResult1 (b1,k1,b2,k2);
+double point2 = PointResult2 (b1,k1,b2,k2);
+Console.WriteLine($"Прямые пересекаются в точке с координатами ({point1};{point2})");
 
 // возвращает введенные значения, либо сообщение об ошибке
 
@@ -41,4 +22,24 @@ int NumberFromUser (string message, string errorMessage)
             return userNumber;
         Console.WriteLine(errorMessage);
     }
+}
+
+// возвращает координату х
+
+double PointResult1 (int bu1, int ku1, int bu2, int ku2)
+{
+double x, z, c;
+z  = bu2-bu1;
+c = ku1-ku2;
+x = z/c;
+return x;
+}
+
+// возвращает координату у
+
+double PointResult2 (int bu1, int ku1, int bu2, int ku2)
+{
+double y;
+y = ku1*point1 + bu1;
+return y;
 }
